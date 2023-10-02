ec2741a1b80535c7392d3aad6ba84d8e2fab29c0
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 16:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function volume_sphere() {
-	    //Write your code here
-	const radiusInput = document.getElementById('radius');
+    //Write your code here
+  const radiusInput = document.getElementById('radius');
   const radius = parseFloat(radiusInput.value);
   if (!isNaN(radius)) {
     const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+    const roundedVolume = volume.toFixed(4);
     const volumeField = document.getElementById('volume');
-    volumeField.value = volume;
+    volumeField.value = roundedVolume;
   }
   return false;  
-	} 
-	
+} 
 window.onload = function() {
   document.getElementById('MyForm').onsubmit = volume_sphere;
 };
